65e72fb314a83903b40b0c5f99aa169d0918c82b
Thomas Gourgues <thomas.gourgues@gmail.com>
2023-07-07 11:28:08 +0200

    Fixed Credentials for 1.2

--- Samples~/SharedScene/App.cs
+++ Samples~/SharedScene/App.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using DolbyIO.Comms;
 using DolbyIO.Comms.Unity;
 
-public class App : MonoBehaviour
+public class App : Credentials
 {
     private DolbyIOSDK _sdk = DolbyIOManager.Sdk;
 
@@ -14,10 +14,6 @@ public class App : MonoBehaviour
     // Start is called before the first frame update
     async void Start()
     {
-        await _sdk.InitAsync(Token, () => {
-            return Token;
-        });
-
         await _sdk.Session.OpenAsync(new UserInfo { Name = "Anonymous" });
 
         _sdk.Conference.ParticipantUpdated += OnParticipantUpdated;
@@ -29,6 +25,11 @@ public class App : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public override string GetToken()
+    {
+        return Token;
     }
 
     private void OnParticipantUpdated(Participant p) 
